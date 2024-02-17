cc0bca666515dd740e6565f0b7f1fd8c6ad531f6
mokuken <imsilencer44@gmail.com>
2024-02-18 10:00:44 +1100

    about page contents added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,37 @@
 function Projects() {
     return (
       <div className="about-container">
-        <h1>Under Construction</h1>
+        <h1>A bit about me</h1>
+        <h2>I'm a self-taught web developer, i love learning tech-stuff and self-improvement. I passionately combine good design, technology, and innovation in all my projects, which I like to accompany from the first idea to release.</h2>
+        <br/>
+        <h1>Technologies i know</h1>
+        <div className="techs">
+          <div>
+            <h3>Languages</h3>
+            <ul>
+              <li>JavaScript + node.js</li>
+              <li>PHP</li>
+              <li>CSS</li>
+              <li>Java</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Databases</h3>
+            <ul>
+              <li>Firebase</li>
+              <li>MySQL</li>
+              <li>MongoDB</li>
+              <li>phpMyAdmin</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Frameworks</h3>
+            <ul>
+              <li>Vite</li>
+              <li>React</li>
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
