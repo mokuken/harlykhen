64aaf23738f8869fe9960398b313ecab6e024bc3
mokuken <imsilencer44@gmail.com>
2024-02-22 21:31:51 +1100

    project box content changed + version control

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,11 +6,11 @@ function Projects() {
       <h1>Some Projects I've Made</h1>
       <div className="repo-container">
         <FetchGithubRepo repoOwner="mokuken" repoName="harlykhen" />
-        <FetchGithubRepo repoOwner="mokuken" repoName="harlykhen-vite" />
+        <FetchGithubRepo repoOwner="mokuken" repoName="upmenu" />
         <FetchGithubRepo repoOwner="mokuken" repoName="librarysystem" />
         <FetchGithubRepo repoOwner="mokuken" repoName="linux-setup" />
         <FetchGithubRepo repoOwner="mokuken" repoName="mdwm" />
-        <FetchGithubRepo repoOwner="mokuken" repoName="upmenu" />
+        <FetchGithubRepo repoOwner="mokuken" repoName="c-wallpaper-e-60" />
       </div>
     </div>
   )
