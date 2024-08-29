eea917658e04a6e8ea24d254cb38f79689438b44
mokuken <imsilencer44@gmail.com>
2024-08-28 23:27:03 -0700

    added profile image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,32 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareFacebook, faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import profile from '../assets/images/profile.png';
 
 function Home() {
-  return (
-    <div className='home-container'>
-        <div className="hero">
-            <h1>Hello there I'm Harly Khen</h1>
-            <div className="profession">
-                <hr/>
-                <h3>Full Stack Developer</h3>
+    return (
+        <div className='home-container'>
+            <div className="socials">
+                <div className="icons">
+                    <a href="#"><FontAwesomeIcon icon={faSquareFacebook} /></a>
+                    <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
+                    <a href="#"><FontAwesomeIcon icon={faSquareGithub} /></a>
+                </div>
             </div>
-            <p>I'm a Computer Science Student in the Philippines, and I'm very passionate and dedicated to my work.</p>
-            <button className="button">Contact Info</button>
-            <button className="button">Download CV</button>
-        </div>
-        <div className="socials">
-            <div className="icons">
-                <a href="#"><FontAwesomeIcon icon={faSquareFacebook} /></a>
-                <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
-                <a href="#"><FontAwesomeIcon icon={faSquareGithub} /></a>
+            <div className="hero">
+                <h1>Hello there I'm Harly Khen</h1>
+                <div className="profession">
+                    <hr/>
+                    <h3>Full Stack Developer</h3>
+                </div>
+                <p>I'm a Computer Science Student in the Philippines, and I'm very passionate and dedicated to my work.</p>
+                <button className="button">Contact Info</button>
+                <button className="button">Download CV</button>
+            </div>
+            <div className="profile">
+                <img src={profile} alt="Profile" />
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home
