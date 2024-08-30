052a21cc55da031cc8e58a471023b7c80b453e4f
mokuken <imsilencer44@gmail.com>
2024-08-30 10:20:48 -0700

    profession added hr on right

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,8 +15,7 @@ function Home() {
             <div className="hero">
                 <h1>Hello there I'm Harly Khen</h1>
                 <div className="profession">
-                    <hr/>
-                    <h3>Full Stack Developer</h3>
+                    <hr/><h3>Full Stack Developer</h3><hr/>
                 </div>
                 <p>
                     I'm a Computer Science Student in the Philippines, and<br/>
